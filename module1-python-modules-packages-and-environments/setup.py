252c0230e855d3bbf971de550c7d06099e2d5715
Jing Qian <qianjing2020@users.noreply.github.com>
2020-11-02 20:55:26 -0500

    added file

---
+++ module1-python-modules-packages-and-environments/setup.py
@@ -0,0 +1,21 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="my-package-name",  # the name that you will install via pip
+    version="1.0",
+    author="Lamb Data",
+    author_email="your@email.com",
+    description="A short demo for package distribution",
+    long_description=long_description,
+    # required if using a md file for long desc
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/qianjing2020/DS-Unit-3-Sprint-1-Software-Engineering/tree/master/module1-python-modules-packages-and-environments",
+    #keywords="",
+    packages=find_packages()  # ["my_lambdata"]
+)
